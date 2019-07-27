18d5906ff105958ec001333a35efbc0849a8d60b
Jacek Lipiec <jacek.b.lipiec@gmail.com>
2019-07-27 04:19:07 +0200

    Add DDD Core initial project

---
+++ ddd-core/src/main/java/eu/venthe/dddcore/DddCoreApplication.java
@@ -0,0 +1,13 @@
+package eu.venthe.dddcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddCoreApplication.class, args);
+    }
+
+}
