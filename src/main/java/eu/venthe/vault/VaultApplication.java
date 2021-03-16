4f44e296e25c9c2f08c0c81db83293bcd5e62e56
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2021-03-16 17:20:30 +0100

    Add initial spring boot project

---
+++ src/main/java/eu/venthe/vault/VaultApplication.java
@@ -0,0 +1,13 @@
+package eu.venthe.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultApplication.class, args);
+	}
+
+}
