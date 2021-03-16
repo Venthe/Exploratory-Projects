1a6cd8533a2877c4f35f6af862f01b9aa67183ad
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2021-03-16 14:41:01 +0100

    Add Spring Boot Project

---
+++ src/main/java/eu/venthe/javadoc/JavadocApplication.java
@@ -0,0 +1,13 @@
+package eu.venthe.javadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadocApplication.class, args);
+	}
+
+}
