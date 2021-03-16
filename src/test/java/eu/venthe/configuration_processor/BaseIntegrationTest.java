1f76baee9ffd5f6a8f93404e77cca806c77c78c7
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2021-03-16 14:39:59 +0100

    Add Configuration Processor Example

---
+++ src/test/java/eu/venthe/configuration_processor/BaseIntegrationTest.java
@@ -0,0 +1,7 @@
+package eu.venthe.configuration_processor;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+public abstract class BaseIntegrationTest {
+}
--- src/test/java/eu/venthe/configuration_processor/ConfigurationProcessorApplicationTests.java
+++ src/test/java/eu/venthe/configuration_processor/ConfigurationProcessorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigurationProcessorApplicationTests {
+class ConfigurationProcessorApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
