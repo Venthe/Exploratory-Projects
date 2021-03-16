53b0f780364d08ce28c9d252e5590ecd31239581
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2021-03-16 13:07:58 +0100

    Add initial Spring Boot project

---
+++ src/main/java/eu/venthe/configuration_processor/ConfigurationProcessorApplication.java
@@ -0,0 +1,13 @@
+package eu.venthe.configuration_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/venthe/configuration_processor/ConfigurationProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.venthe.configuration_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
