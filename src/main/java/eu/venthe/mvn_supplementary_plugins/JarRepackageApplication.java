f656af7df4b0606f0f2e9a74bcc02dcb299ab82b
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2021-04-12 02:06:44 +0200

    Initial commit

---
+++ src/main/java/eu/venthe/mvn_supplementary_plugins/JarRepackageApplication.java
@@ -0,0 +1,13 @@
+package eu.venthe.mvn_supplementary_plugins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarRepackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarRepackageApplication.class, args);
+	}
+
+}
