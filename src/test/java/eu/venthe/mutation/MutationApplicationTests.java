4246e9bdc6f3edb710d109cc271df14d1e655924
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2021-05-17 23:56:11 +0200

    Add initial commit

---
+++ src/test/java/eu/venthe/mutation/MutationApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.venthe.mutation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
